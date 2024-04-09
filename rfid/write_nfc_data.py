2032629202717709b896d42d8bec890df2824a1e
Nino <fazernino@gmail.com>
2024-04-10 00:11:47 +0200

    modified:   rfid/write_nfc_data.py

--- rfid/write_nfc_data.py
+++ rfid/write_nfc_data.py
@@ -1,25 +1,35 @@
 from pn532 import PN532_SPI
 import json
+import time
 
 def write_to_tag(pn532, data):
     try:
-        print("Écriture des données dans le tag NFC...")
+        print("Attente de la détection du tag NFC...")
         # Configuration pour détecter le tag NFC
         pn532.SAM_configuration()
 
         # Détection du tag NFC
-        uid = pn532.read_passive_target(timeout=0.5)
+        uid = pn532.read_passive_target(timeout=60)  # Attente de 60 secondes
         if uid is None:
-            print("Aucun tag NFC détecté.")
-            return
-        
-        # Lecture du numéro de bloc à écrire
-        block_number = 4  # Modifier le numéro de bloc si nécessaire
+            print("Aucun tag NFC détecté après 1 minute.")
+            while True:
+                choix = input("Voulez-vous réessayer ? (yes/no) : ")
+                if choix.lower() == "yes":
+                    print("Réessayer la détection du tag NFC...")
+                    return write_to_tag(pn532, data)
+                elif choix.lower() == "no":
+                    print("Arrêt du programme.")
+                    return
+                else:
+                    print("Choix invalide. Veuillez répondre par 'yes' ou 'no'.")
+        else:
+            # Lecture du numéro de bloc à écrire
+            block_number = 4  # Modifier le numéro de bloc si nécessaire
 
-        # Écriture des données dans le tag NFC
-        pn532.mifare_classic_write_block(block_number, data)
+            # Écriture des données dans le tag NFC
+            pn532.mifare_classic_write_block(block_number, data)
 
-        print("Données écrites avec succès dans le tag NFC.")
+            print("Données écrites avec succès dans le tag NFC.")
     except Exception as e:
         print("Erreur lors de l'écriture dans le tag NFC :", e)
 
@@ -48,6 +58,8 @@ def get_data_from_user():
 
 if __name__ == '__main__':
     try:
+        print('Ecriture de données dans un tag NFC ...')
+
         # Initialisation du module NFC PN532
         pn532 = PN532_SPI(debug=False, reset=20, cs=4)
 
