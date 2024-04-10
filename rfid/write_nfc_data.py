90ed806daa74243faab1fd14281484e19257baf0
Nino <fazernino@gmail.com>
2024-04-10 03:03:52 +0200

    modified:   rfid/write_nfc_data.py

--- rfid/write_nfc_data.py
+++ rfid/write_nfc_data.py
@@ -18,7 +18,7 @@ def write_to_tag(pn532, uid, data):
 
         # Écrire les données dans le bloc
         print("Écriture des données dans le bloc...")
-        pn532.mifare_classic_write_block(uid, block_number, binascii.unhexlify(data_hex))
+        pn532.mifare_classic_write_block(uid,block_number, binascii.unhexlify(data_hex))
 
         print("Données écrites avec succès dans le tag NFC.")
         return True
