db08887912cdbaabf957ed8a6f3e93c4479b3a3d
Nino <fazernino@gmail.com>
2024-04-12 13:04:57 +0200

    update w

--- rfid/write_nfc_data.py
+++ rfid/write_nfc_data.py
@@ -6,25 +6,35 @@ def write_to_tag(pn532, uid, data):
     try:
         block_number = 6
         key_a = b'\xFF\xFF\xFF\xFF\xFF\xFF'
+        print("Chargement d'écriture des données dans le tag NFC...")
+
         # Assurer que les données font exactement 16 octets
         data_bytes = data.ljust(16, b'\0')[:16]
+
+        print("Côté écriture : Authentification du bloc...")
         pn532.mifare_classic_authenticate_block(uid, block_number=block_number, key_number=nfc.MIFARE_CMD_AUTH_A, key=key_a)
+        
+        print("Côté écriture : Écriture des données dans le bloc...")
         pn532.mifare_classic_write_block(block_number, data_bytes)
         if pn532.mifare_classic_read_block(block_number) == data_bytes:
-            print('Écriture réussie sur le bloc %d.' % block_number)
+            print('Côté écriture : Écriture réussie sur le bloc %d.' % block_number)
             return True
         else:
-            print('Erreur lors de la lecture des données écrites.')
+            print('Côté écriture : Erreur lors de la lecture des données écrites.')
             return False
     except Exception as e:
-        print('Erreur lors de l\'écriture dans le tag NFC :', e)
+        print('Côté écriture : Erreur lors de l\'écriture dans le tag NFC :', e)
         return False
 
 def read_from_tag(pn532, uid, block_number):
     try:
+        print("Côté lecture : Authentification du bloc...")
         pn532.mifare_classic_authenticate_block(uid, block_number=block_number, key_number=nfc.MIFARE_CMD_AUTH_A, key=b'\xFF\xFF\xFF\xFF\xFF\xFF')
+        
+        print("Côté lecture : Lecture des données du bloc...")
         data_read = pn532.mifare_classic_read_block(block_number)
-        print('Lecture réussie sur le bloc %d : %s' % (block_number, data_read))
+        
+        print('Côté lecture : Lecture réussie sur le bloc %d : %s' % (block_number, data_read))
         return data_read
     except Exception as e:
         print('Erreur lors de la lecture du tag NFC :', e)
