7f8f2209b63ff04ccd86a7a37098081a8a58850f
Nino <fazernino@gmail.com>
2024-04-12 02:19:15 +0200

    modified:   rfid/write_nfc_data.py

--- rfid/write_nfc_data.py
+++ rfid/write_nfc_data.py
@@ -20,6 +20,16 @@ def write_to_tag(pn532, uid, data):
         print('Erreur lors de l\'écriture dans le tag NFC :', e)
         return False
 
+def read_from_tag(pn532, uid, block_number):
+    try:
+        pn532.mifare_classic_authenticate_block(uid, block_number=block_number, key_number=nfc.MIFARE_CMD_AUTH_A, key=b'\xFF\xFF\xFF\xFF\xFF\xFF')
+        data_read = pn532.mifare_classic_read_block(block_number)
+        print('Lecture réussie sur le bloc %d : %s' % (block_number, data_read))
+        return data_read
+    except Exception as e:
+        print('Erreur lors de la lecture du tag NFC :', e)
+        return None
+
 if __name__ == '__main__':
     try:
         # Initialisation du module NFC PN532
@@ -45,6 +55,13 @@ if __name__ == '__main__':
         # Écrire sur la carte RFID/NFC
         if write_to_tag(pn532, uid, data_to_write.encode()):
             print("Écriture réussie sur la carte RFID/NFC.")
+
+            # Lire à nouveau les données écrites pour vérification
+            read_data = read_from_tag(pn532, uid, block_number=6)
+            if read_data is not None:
+                print("Données lues depuis la carte RFID/NFC :", read_data.decode())
+            else:
+                print("Échec de la lecture des données depuis la carte RFID/NFC.")
         else:
             print("Échec de l'écriture sur la carte RFID/NFC.")
             
