bd3da12339663e529e31e484450eed90325f752b
Nino <fazernino@gmail.com>
2024-04-15 15:15:04 +0200

    test_écriture 2 blocks

--- rfid/write_main.py
+++ rfid/write_main.py
@@ -1,6 +1,6 @@
-from pn532 import PN532_SPI
-from write_tokenId import WriteTokenID
-from write_date import WriteDate
+import datetime
+import write_date  # Importer le fichier contenant les fonctions pour écrire la date
+import write_nfc_token    # Importer le fichier contenant les fonctions pour écrire l'ID de token NFC
 
 if __name__ == '__main__':
     try:
@@ -22,29 +22,33 @@ if __name__ == '__main__':
                 print("Tag NFC détecté avec l'UID suivant : ", [hex(i) for i in uid])
                 print("Permission d'écriture autorisée ...")
 
+                # Demander à l'utilisateur d'entrer les données à écrire dans la carte RFID/NFC
+                data_write_date = datetime.datetime.now().strftime('%Y-%m-%d')
+                data_to_write = input("Entrez les données à écrire dans le tag NFC (16 octets) : NFT_tokenID : ")
+
                 # Écrire la date sur la carte RFID/NFC
-                if WriteDate.write_to_tag(pn532, uid):
+                if write_date.write_to_tag(pn532, uid, data_write_date.encode()):
                     print("Écriture de la date réussie sur la carte RFID/NFC.")
                 else:
                     print("Échec de l'écriture de la date sur la carte RFID/NFC.")
 
                 # Écrire l'ID de token NFC sur la carte RFID/NFC
-                if WriteTokenID.write_to_tag(pn532, uid):
+                if write_nfc_token.write_to_tag(pn532, uid, data_to_write.encode()):
                     print("Écriture de l'ID de token NFC réussie sur la carte RFID/NFC.")
                 else:
                     print("Échec de l'écriture de l'ID de token NFC sur la carte RFID/NFC.")
 
                 # Lire à nouveau les données écrites pour vérification
-                read_date = WriteDate.read_from_tag(pn532, uid)
-                read_token = WriteTokenID.read_from_tag(pn532, uid)
+                read_date = write_date.read_from_tag(pn532, uid)
+                read_id = write_nfc_token.read_from_tag(pn532, uid)
 
                 if read_date is not None:
                     print("Date lue depuis la carte RFID/NFC :", read_date.decode())
                 else:
                     print("Échec de la lecture de la date depuis la carte RFID/NFC.")
 
-                if read_token is not None:
-                    print("ID de token NFC lu depuis la carte RFID/NFC :", read_token.decode())
+                if read_id is not None:
+                    print("ID de token NFC lu depuis la carte RFID/NFC :", read_id.decode())
                 else:
                     print("Échec de la lecture de l'ID de token NFC depuis la carte RFID/NFC.")
 
--- rfid/write_nfc_token.py
+++ rfid/write_nfc_token.py
@@ -1,50 +1,49 @@
 import RPi.GPIO as GPIO
 import pn532.pn532 as nfc
-import datetime
-
 from pn532 import PN532_SPI
 
-def write_to_tag(pn532, uid, data_date):
+def write_to_tag(pn532, uid, data):
     try:
+        block_number = 10
         key_a = b'\xFF\xFF\xFF\xFF\xFF\xFF'
         print("Chargement d'écriture des données dans le tag NFC...")
 
         # Assurer que les données font exactement 16 octets
-        data_bytes_date = data_date.ljust(16, b'\0')[:16]
+        data_bytes = data.ljust(16, b'\0')[:16]
 
         print("Côté écriture : Authentification du bloc...")
-        pn532.mifare_classic_authenticate_block(uid, block_number=13, key_number=nfc.MIFARE_CMD_AUTH_A, key=key_a)
-
-        print("Côté écriture : Écriture des données dans le bloc 13")
-        pn532.mifare_classic_write_block(13, data_bytes_date)
-
-        if pn532.mifare_classic_read_block(13) == data_bytes_date:
-            print('Côté écriture : Écriture réussie sur le bloc 13.')
+        pn532.mifare_classic_authenticate_block(uid, block_number=block_number, key_number=nfc.MIFARE_CMD_AUTH_A, key=key_a)
+        
+        print("Côté écriture : Écriture des données dans le bloc...")
+        pn532.mifare_classic_write_block(block_number, data_bytes)
+        if pn532.mifare_classic_read_block(block_number) == data_bytes:
+            print('Côté écriture : Écriture réussie sur le bloc %d.' % block_number)
             return True
         else:
-            print('Côté écriture : Erreur lors de la lecture des données écrites dans le bloc 13.')
+            print('Côté écriture : Erreur lors de la lecture des données écrites.')
             return False
     except Exception as e:
         print('Côté écriture : Erreur lors de l\'écriture dans le tag NFC :', e)
         return False
 
-def read_from_tag(pn532, uid):
+def read_from_tag(pn532, uid, block_number):
     try:
         print("Côté lecture : Authentification du bloc...")
-        pn532.mifare_classic_authenticate_block(uid, block_number=13, key_number=nfc.MIFARE_CMD_AUTH_A, key=b'\xFF\xFF\xFF\xFF\xFF\xFF')
-
+        pn532.mifare_classic_authenticate_block(uid, block_number=block_number, key_number=nfc.MIFARE_CMD_AUTH_A, key=b'\xFF\xFF\xFF\xFF\xFF\xFF')
+        
         print("Côté lecture : Lecture des données du bloc...")
-        data_read_date = pn532.mifare_classic_read_block(13)
-
-        print('Côté lecture : Lecture réussie sur le bloc 13 : %r' % data_read_date)
-        return data_read_date
+        data_read = pn532.mifare_classic_read_block(block_number)
+        
+        print('Côté lecture : Lecture réussie sur le bloc %d : %s' % (block_number, data_read))
+        return data_read
     except Exception as e:
         print('Erreur lors de la lecture du tag NFC :', e)
         return None
 
-def get_date():
-    date_now = datetime.datetime.now().strftime('%Y-%m-%d')
-    return date_now
+def get_data_from_user():
+    print("Entrez les données à écrire dans le tag NFC (16 octets) :", end=" ")
+    data_to_write = input("NFT_tokenID : ")
+    return data_to_write
 
 if __name__ == '__main__':
     try:
@@ -52,7 +51,7 @@ if __name__ == '__main__':
         pn532 = PN532_SPI(debug=False, reset=20, cs=4)
         ic, ver, rev, support = pn532.get_firmware_version()
         print('Module NFC PN532 trouvé avec la version de firmware : {0}.{1}'.format(ver, rev))
-
+        
         while True:
             # Configuration pour communiquer avec les cartes MiFare
             pn532.SAM_configuration()
@@ -62,20 +61,21 @@ if __name__ == '__main__':
             uid = pn532.read_passive_target(timeout=0.5)
             print('.', end="")
             # Réessayer si aucune carte n'est disponible.
-            if uid is not None:
+            if uid is not None:    
                 print("Tag NFC détecté avec l'UID suivant : ", [hex(i) for i in uid])
                 print("Permission d'écriture autorisée ...")
 
-                data_write_date = input("Entrez la date à écrire sur la carte RFID/NFC (format : YYYY-MM-DD) : ")
+                # Demander à l'utilisateur d'entrer les données à écrire dans la carte RFID/NFC
+                data_to_write = get_data_from_user()
 
                 # Écrire sur la carte RFID/NFC
-                if write_to_tag(pn532, uid, data_write_date.encode()):
+                if write_to_tag(pn532, uid, data_to_write.encode()):
                     print("Écriture réussie sur la carte RFID/NFC.")
 
                     # Lire à nouveau les données écrites pour vérification
-                    read_data = read_from_tag(pn532, uid)
+                    read_data = read_from_tag(pn532, uid, block_number=10)
                     if read_data is not None:
-                        print("Données lues depuis la carte RFID/NFC :", read_data[0].decode(), read_data[1].decode())
+                        print("Données lues depuis la carte RFID/NFC :", read_data.decode())
                         break
                     else:
                         print("Échec de la lecture des données depuis la carte RFID/NFC.")
@@ -93,7 +93,7 @@ if __name__ == '__main__':
                         exit()
                     else:
                         print("Choix invalide. Veuillez répondre par 'yes' ou 'no'.")
-
+            
     except Exception as e:
         print(e)
     finally:
