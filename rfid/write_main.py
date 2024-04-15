cd0d3341722efacbd33967060a9b0c4a32adec1c
Nino <fazernino@gmail.com>
2024-04-15 15:18:00 +0200

    test_écriture 2 blocks

--- rfid/write_main.py
+++ rfid/write_main.py
@@ -3,6 +3,8 @@ import RPi.GPIO as GPIO
 import write_date  # Importer le fichier contenant les fonctions pour écrire la date
 import write_nfc_token    # Importer le fichier contenant les fonctions pour écrire l'ID de token NFC
 
+from pn532 import PN532_SPI
+
 if __name__ == '__main__':
     try:
         # Initialisation du module NFC PN532
