85d28d7dce8d6bf7c6aad5baa44843b98f16d70e
Nino <fazernino@gmail.com>
2024-04-15 14:00:46 +0200

    test_écriture date

--- rfid/read_date.py
+++ rfid/read_date.py
@@ -13,16 +13,16 @@ def write_to_tag(pn532, uid, data_date):
         data_bytes_date = data_date.ljust(16, b'\0')[:16]
 
         print("Côté écriture : Authentification du bloc...")
-        pn532.mifare_classic_authenticate_block(uid, block_number=14, key_number=nfc.MIFARE_CMD_AUTH_A, key=key_a)
+        pn532.mifare_classic_authenticate_block(uid, block_number=13, key_number=nfc.MIFARE_CMD_AUTH_A, key=key_a)
 
-        print("Côté écriture : Écriture des données dans le bloc 14")
-        pn532.mifare_classic_write_block(14, data_bytes_date)
+        print("Côté écriture : Écriture des données dans le bloc 13")
+        pn532.mifare_classic_write_block(13, data_bytes_date)
 
-        if pn532.mifare_classic_read_block(14) == data_bytes_date:
-            print('Côté écriture : Écriture réussie sur le bloc 14.')
+        if pn532.mifare_classic_read_block(13) == data_bytes_date:
+            print('Côté écriture : Écriture réussie sur le bloc 13.')
             return True
         else:
-            print('Côté écriture : Erreur lors de la lecture des données écrites dans le bloc 14.')
+            print('Côté écriture : Erreur lors de la lecture des données écrites dans le bloc 13.')
             return False
     except Exception as e:
         print('Côté écriture : Erreur lors de l\'écriture dans le tag NFC :', e)
@@ -31,12 +31,12 @@ def write_to_tag(pn532, uid, data_date):
 def read_from_tag(pn532, uid):
     try:
         print("Côté lecture : Authentification du bloc...")
-        pn532.mifare_classic_authenticate_block(uid, block_number=14, key_number=nfc.MIFARE_CMD_AUTH_A, key=b'\xFF\xFF\xFF\xFF\xFF\xFF')
+        pn532.mifare_classic_authenticate_block(uid, block_number=13, key_number=nfc.MIFARE_CMD_AUTH_A, key=b'\xFF\xFF\xFF\xFF\xFF\xFF')
 
         print("Côté lecture : Lecture des données du bloc...")
-        data_read_date = pn532.mifare_classic_read_block(14)
+        data_read_date = pn532.mifare_classic_read_block(13)
 
-        print('Côté lecture : Lecture réussie sur le bloc 14 : %r' % data_read_date)
+        print('Côté lecture : Lecture réussie sur le bloc 13 : %r' % data_read_date)
         return data_read_date
     except Exception as e:
         print('Erreur lors de la lecture du tag NFC :', e)
