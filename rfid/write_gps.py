b560ad4318638058f6de0cfbbc6c6de670a7a53c
qtambone <quintinobello91@gmail.com>
2024-07-01 11:15:36 +0200

    fix

--- rfid/write_gps.py
+++ rfid/write_gps.py
@@ -61,7 +61,7 @@ def get_gps():
     data = gps.read_data()
     if data is not None:
         print("GPS read")
-        print(str(data))
+        print("??????" , str(data))
         return str(data)
     else:
         print("GPS not read")
